b24a0a86ac7a7d836fa404041eb7b96c1121176d
noahcassutti <noahncassutti@gmail.com>
2023-04-16 06:25:48 -0300

    creacion de pag petshop
    creacion de pagina de petshop, el css y jss. Intentando hacer la API

---
+++ JS/petshop.js
@@ -0,0 +1,13 @@
+let precios = [];
+
+const consultarApiPrecios = async () =>{
+    const data = await fetch ("https://amazon-product-price-data.p.rapidapi.com/product");
+    const dataParse = await data.json();
+    return dataParse
+}
+
+const tests = async () =>{
+    console.log(precios.data)
+
+
+    const preciosData = 
