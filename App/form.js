60108e353c59a4c4911139a275f5977c07cfc9a6
U01-059-JUAN-C <U01-059-JUAN-C@icon.loc>
2023-04-17 16:51:27 -0300

    termine el html y css de los form y empece con js

---
+++ App/form.js
@@ -0,0 +1,11 @@
+let d = document;
+
+let $nombre = d.getElementById("input-nombre")
+let $apellido = d.getElementById("input-apellido")
+let $email = d.getElementById("input-email")
+let $tel = d.getElementById("input-telefono")
+let $provincia = d.getElementById("input-provincia")
+let $ciudad = d.getElementById("input-ciudad")
+let $optionAdoption = d.getElementsByClassName("check-adopcion")
+let $tengoMasc = d.getElementsByClassName("tengo-noTengo")
+let $debSaber = d.getElementsByClassName("deb-saber")
