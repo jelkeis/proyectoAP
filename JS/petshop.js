7c02231b2b0f6a5d10dc4e37b460b4813c43bfeb
noahcassutti <noahncassutti@gmail.com>
2023-04-17 01:46:41 -0300

    elimine el intento de api y lo de js

--- JS/petshop.js
+++
@@ -1,6 +0,0 @@
-const apiUrl = "https://api.mercadolibre.com/sites/MLA/search?q=Alimentoperro"
-
-
-await fetch(`${apiUrl}/usuarios`)
-  .then(response => response.json())
-  .then(data => console.log(data.results))
