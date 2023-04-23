8c3c4c96a375546487ec804cbb8cec21de4fd508
JoacoAlessi <joacotoms@gmail.com>
2023-04-23 17:44:59 -0300

    Correcciones header y form
    Corregi el header en la seccion de dispositivos chicos para que el logo fuese al inicio.
    
    Form rehice el JS para que la validacion fuese completa.

--- App/form.js
+++ App/form.js
@@ -1,114 +1,280 @@
-let d = document;
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
 
-let $nombre = d.getElementById("input-nombre")
-let $apellido = d.getElementById("input-apellido")
-let $email = d.getElementById("input-email")
-let $tel = d.getElementById("input-telefono")
-let $provincia = d.getElementById("input-provincia")
-let $ciudad = d.getElementById("input-ciudad")
-let $optionAdoption = d.querySelectorAll('input[type = "checkbox"]')
-let $tengoMasc = d.getElementsByName("opciones")
-// let $debSaber = d.getElementById("input-debSaber")
-let $comoHogar = d.getElementById("input-comoHogar")
-// let $duda = d.getElementById("input-dudas")
-let $pHogar = d.getElementById("p-hogar")
-let $pMascotasSiNo = d.getElementById("mascotas-sino")
-let $pQueAdopto = d.getElementById("que-adoptar")
-let $pCiudad = d.getElementById("p-ciudad")
-let $pProvincia = d.getElementById("p-provincia")
-let $pTelefono = d.getElementById("p-telefono")
-let $pEmail = d.getElementById("p-email")
-let $pApe = d.getElementById("p-ape")
-let $pNom = d.getElementById("p-name")
-let alMenosUnCheckbox = false
-let alMenosUnRadio = false
-let validacion
-let $sendBtn = d.getElementById("send-btn")
-let $cancelBtn = d.getElementById("cancel-btn")
+const expresiones = {
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  telefono: /^\d{7,14}$/,
+  lugar: /^[A-Za-z0-9\s]{4,99}$/,
+};
 
-function mostrarAviso(esteElemento) {
-    esteElemento.style.display = "block"
-}
+const campos = {
+  nombre: false,
+  apellido: false,
+  email: false,
+  telefono: false,
+  provincia: false,
+  ciudad: false,
+};
 
-function validar() {
-    validacion = true
-    if ($nombre.value == "") {
-        validacion = false
-        mostrarAviso($pNom)
-    }
+// VALIDACION DEL FORMULARIO
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "nombre":
+      if (expresiones.nombre.test(e.target.value)) {
+        document
+          .getElementById("grupo-nombre")
+          .classList.remove("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-nombre")
+          .classList.add("form-grupo-correcto");
+        document
+          .querySelector("#grupo-nombre i")
+          .classList.remove("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-nombre i")
+          .classList.add("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-nombre .form-input-error")
+          .classList.remove("form-input-error-activo");
+        campos["nombre"] = true;
+      } else {
+        document
+          .getElementById("grupo-nombre")
+          .classList.add("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-nombre")
+          .classList.remove("form-grupo-correcto");
+        document
+          .querySelector("#grupo-nombre i")
+          .classList.add("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-nombre i")
+          .classList.remove("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-nombre .form-input-error")
+          .classList.add("form-input-error-activo");
+        campos["nombre"] = false;
+      }
+      break;
+    case "apellido":
+      if (expresiones.nombre.test(e.target.value)) {
+        document
+          .getElementById("grupo-apellido")
+          .classList.remove("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-apellido")
+          .classList.add("form-grupo-correcto");
+        document
+          .querySelector("#grupo-apellido i")
+          .classList.remove("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-apellido i")
+          .classList.add("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-apellido .form-input-error")
+          .classList.remove("form-input-error-activo");
+        campos["apellido"] = true;
+      } else {
+        document
+          .getElementById("grupo-apellido")
+          .classList.add("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-apellido")
+          .classList.remove("form-grupo-correcto");
+        document
+          .querySelector("#grupo-apellido i")
+          .classList.add("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-apellido i")
+          .classList.remove("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-apellido .form-input-error")
+          .classList.add("form-input-error-activo");
+        campos["apellido"] = false;
+      }
+      break;
+    case "email":
+      if (expresiones.email.test(e.target.value)) {
+        document
+          .getElementById("grupo-mail")
+          .classList.remove("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-mail")
+          .classList.add("form-grupo-correcto");
+        document
+          .querySelector("#grupo-mail i")
+          .classList.remove("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-mail i")
+          .classList.add("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-mail .form-input-error")
+          .classList.remove("form-input-error-activo");
+        campos["mail"] = true;
+      } else {
+        document
+          .getElementById("grupo-mail")
+          .classList.add("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-mail")
+          .classList.remove("form-grupo-correcto");
+        document
+          .querySelector("#grupo-mail i")
+          .classList.add("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-mail i")
+          .classList.remove("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-mail .form-input-error")
+          .classList.add("form-input-error-activo");
+        campos["mail"] = false;
+      }
+      break;
+    case "provincia":
+      if (expresiones.lugar.test(e.target.value)) {
+        document
+          .getElementById("grupo-provincia")
+          .classList.remove("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-provincia")
+          .classList.add("form-grupo-correcto");
+        document
+          .querySelector("#grupo-provincia i")
+          .classList.remove("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-provincia i")
+          .classList.add("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-provincia .form-input-error")
+          .classList.remove("form-input-error-activo");
+        campos["provincia"] = true;
+      } else {
+        document
+          .getElementById("grupo-provincia")
+          .classList.add("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-provincia")
+          .classList.remove("form-grupo-correcto");
+        document
+          .querySelector("#grupo-provincia i")
+          .classList.add("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-provincia i")
+          .classList.remove("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-provincia .form-input-error")
+          .classList.add("form-input-error-activo");
+        campos["provincia"] = false;
+      }
+      break;
+    case "ciudad":
+      if (expresiones.lugar.test(e.target.value)) {
+        document
+          .getElementById("grupo-ciudad")
+          .classList.remove("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-ciudad")
+          .classList.add("form-grupo-correcto");
+        document
+          .querySelector("#grupo-ciudad i")
+          .classList.remove("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-ciudad i")
+          .classList.add("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-ciudad .form-input-error")
+          .classList.remove("form-input-error-activo");
+        campos["ciudad"] = true;
+      } else {
+        document
+          .getElementById("grupo-ciudad")
+          .classList.add("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-ciudad")
+          .classList.remove("form-grupo-correcto");
+        document
+          .querySelector("#grupo-ciudad i")
+          .classList.add("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-ciudad i")
+          .classList.remove("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-ciudad .form-input-error")
+          .classList.add("form-input-error-activo");
+        campos["ciudad"] = false;
+      }
+      break;
+    case "telefono":
+      if (expresiones.telefono.test(e.target.value)) {
+        document
+          .getElementById("grupo-telefono")
+          .classList.remove("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-telefono")
+          .classList.add("form-grupo-correcto");
+        document
+          .querySelector("#grupo-telefono i")
+          .classList.remove("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-telefono i")
+          .classList.add("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-telefono .form-input-error")
+          .classList.remove("form-input-error-activo");
+        campos["telefono"] = true;
+      } else {
+        document
+          .getElementById("grupo-telefono")
+          .classList.add("form-grupo-incorrecto");
+        document
+          .getElementById("grupo-telefono")
+          .classList.remove("form-grupo-correcto");
+        document
+          .querySelector("#grupo-telefono i")
+          .classList.add("bi-x-circle-fill");
+        document
+          .querySelector("#grupo-telefono i")
+          .classList.remove("bi-check-circle-fill");
+        document
+          .querySelector("#grupo-telefono .form-input-error")
+          .classList.add("form-input-error-activo");
+        campos["telefono"] = false;
+      }
+      break;
+  }
+};
 
-    if ($apellido.value == "") {
-        validacion = false
-        mostrarAviso($pApe)
-    }
+// EVENT LISTENERS
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if ($email.value == "") {
-        validacion = false
-        mostrarAviso($pEmail)
-    }
+  if (
+    campos.nombre &&
+    campos.apellido &&
+    campos.telefono &&
+    campos.provincia &&
+    campos.ciudad
+  ) {
+    formulario.reset();
 
-    if ($tel.value == "") {
-        validacion = false
-        mostrarAviso($pTelefono)
-    }
+    document
+      .getElementById("form-mensaje-exito")
+      .classList.add("form-mensaje-exito-activo");
+    setTimeout(() => {
+      document
+        .getElementById("form-mensaje-exito")
+        .classList.remove("form-mensaje-exito-activo");
+    }, 3000);
+    document.querySelectorAll(".form-grupo-correcto").forEach((icono) => {
+      icono.classList.remove("form-grupo-correcto");
+    });
+  } else {
+    document.getElementById('form-mensaje').classList.add('form-mensaje-activo')
+  }
+});
 
-    if ($provincia.value == "") {
-        validacion = false
-        mostrarAviso($pProvincia)
-    }
-
-    if ($ciudad.value == "") {
-        validacion = false
-        mostrarAviso($pCiudad)
-    }
-
-    for(let i = 0; i < $optionAdoption.length; i++){
-        if($optionAdoption[i].checked){
-            alMenosUnCheckbox = true
-            break
-        }
-     }
-     if(alMenosUnCheckbox == false){
-        validacion = false
-        mostrarAviso($pQueAdopto)
-     }
-
-     for(let i = 0; i < $tengoMasc.length; i++){
-        if($tengoMasc[i].checked){
-            alMenosUnRadio = true
-            break
-        }
-     }
-     if(alMenosUnRadio == false){
-        validacion = false
-        mostrarAviso($pMascotasSiNo)
-     }
-
-     if($comoHogar.value == ""){
-        validacion = false
-        mostrarAviso($pHogar)
-     }
-}
-
-function redireccion(){
-    window.location.href = "../index.html"
-}
-
-function enviarCuestionario(){
-    validar();
-    if(validacion == true){
-        alert("El formulario fue enviado con éxito, serás redirigido a la página de Inicio")
-        setTimeout(redireccion, 1000)
-    }
-}
-
-function cancelarCuestionario(){
-    alert("El cuestionario fue cancelado, serás redirigido a la página de Inicio")
-    setTimeout(redireccion, 1000)
-}
-
-
-// enviarCuestionario()
-
-$sendBtn.addEventListener("click",enviarCuestionario)
-$cancelBtn.addEventListener("click",cancelarCuestionario)
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+});
